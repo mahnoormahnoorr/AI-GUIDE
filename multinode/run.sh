13859a86356539f01cb7720b7ba234a320ab0daf
Mahnoor Mahnoor <mahnoor.mahnoor@csc.fi>
2026-02-10 16:48:28 +0200

    Update run.sh

--- multinode/run.sh
+++ multinode/run.sh
@@ -12,7 +12,7 @@ export MASTER_ADDR MASTER_PORT
   srun --nodes=$SLURM_JOB_NUM_NODES --ntasks-per-node=1 --gpus-per-node=8 --cpu-bind=none \
   singularity exec "$SIF" bash -lc "
     $WITH_CONDA
-    source visiontransformer-env/bin/activate
+    source h5-env/bin/activate
     python -m torch.distributed.run \
       --nnodes=$SLURM_JOB_NUM_NODES \
       --nproc_per_node=8 \
