d7d97492bc13193f2ed30421fca6e98423951403
Mahnoor Mahnoor <mahnoor.mahnoor@csc.fi>
2026-02-10 10:17:08 +0200

    Create run.sh

---
+++ Quickstart/run.sh
@@ -0,0 +1,7 @@
+module use /appl/local/containers/ai-modules
+module load singularity-AI-bindings
+export SIF=/appl/local/laifs/containers/lumi-multitorch-u24r64f21m43t29-20260124_092648/lumi-multitorch-full-u24r64f21m43t29-20260124_092648.sif
+
+python -m venv h5-env --system-site-packages
+source h5-env/bin/activate
+(h5-env)> pip install h5py
