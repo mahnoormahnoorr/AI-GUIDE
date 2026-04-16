5f972eeda1da8c9b00303b6fb9b92d607680dfdf
Mahnoor Mahnoor <mahnoor.mahnoor@csc.fi>
2026-04-16 10:26:05 +0300

    Update run.sh

--- Quickstart/run.sh
+++ Quickstart/run.sh
@@ -1,6 +1,6 @@
 module use /appl/local/containers/ai-modules
 module load singularity-AI-bindings
-export SIF=/appl/local/laifs/containers/lumi-multitorch-u24r64f21m43t29-20260124_092648/lumi-multitorch-full-u24r64f21m43t29-20260124_092648.sif
+export SIF=/appl/local/laifs/containers/lumi-multitorch-u24r64f21m43t29-20260319_153422/lumi-multitorch-full-u24r64f21m43t29-20260319_153422.sif
 python3 -m venv h5-env --system-site-packages
 source h5-env/bin/activate
 (h5-env)> pip install h5py
